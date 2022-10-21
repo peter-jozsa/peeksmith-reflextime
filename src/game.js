4d09002f2fe4de5b656a5b0686ae622b0e7d91bb
Balázs Brezina <brezinabalazs@gmail.com>
2022-10-21 15:08:27 +0200

    resolve mr

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 const BUTTONS = {
-  LEFT_BUTTON: 'LEFT_BUTTON',
-  RIGHT_BUTTON: 'RIGHT_BUTTON'
-}
+  LEFT_BUTTON: "LEFT_BUTTON",
+  RIGHT_BUTTON: "RIGHT_BUTTON",
+};
 
 const BUTTON = {
   isb1: BUTTONS.LEFT_BUTTON,
@@ -20,16 +20,14 @@ const getButton = (incomingBtnState) => {
   return { side: BUTTON[button], state };
 };
 
-
 class ReflexTimeGame {
-
   constructor(display, { rounds = 5 } = {}) {
-    this.display = display
-    this.maxRounds = rounds
-    this.reactionTimesMs = []
+    this.display = display;
+    this.maxRounds = rounds;
+    this.reactionTimesMs = [];
     this._onPressedButton = null;
 
-    this.initDisplay()
+    this.initDisplay();
   }
 
   initDisplay() {
@@ -37,75 +35,78 @@ class ReflexTimeGame {
       const { side, state } = getButton(message);
 
       if (state === BUTTON_STATES.PRESS && this._onPressedButton) {
-        this._onPressedButton(side)
+        this._onPressedButton(side);
       }
     });
   }
 
   randomizeExpectedButton() {
-    return Math.random() <= 0.5 ? BUTTONS.LEFT_BUTTON : BUTTONS.RIGHT_BUTTON
+    return Math.random() <= 0.5 ? BUTTONS.LEFT_BUTTON : BUTTONS.RIGHT_BUTTON;
   }
 
   displayExpectation(expectedButton) {
     if (expectedButton === BUTTONS.LEFT_BUTTON) {
-      this.display.displayText('LEFT')
+      this.display.displayText("LEFT");
     } else {
-      this.display.displayText('RIGHT')
+      this.display.displayText("RIGHT");
     }
+    display.send("/VL211\n~....\n");
   }
 
   async waitForButtonPress() {
     // TODO
     return new Promise((resolve, reject) => {
       this._onPressedButton = (pressedButton) => {
-        resolve(pressedButton)
-        this._onPressedButton = null
-      }
-    })
+        resolve(pressedButton);
+        this._onPressedButton = null;
+      };
+    });
   }
 
   async measureReactionTime() {
-    const expectedButton = this.randomizeExpectedButton()
-    
-    this.displayExpectation(expectedButton)
-    const startTime = Date.now()
-    const pressedButton = await this.waitForButtonPress(expectedButton)
-    const endTime = Date.now()
+    const expectedButton = this.randomizeExpectedButton();
 
-    const tookMs = endTime - startTime
-    const matched = pressedButton === expectedButton
+    this.displayExpectation(expectedButton);
+    const startTime = Date.now();
+    const pressedButton = await this.waitForButtonPress(expectedButton);
+    const endTime = Date.now();
 
-    return { tookMs, matched }
+    const tookMs = endTime - startTime;
+    const matched = pressedButton === expectedButton;
+
+    return { tookMs, matched };
   }
 
   sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
   async countdown(from = 3) {
-    for (let current = from; current>0; current--) {
-      this.display.displayText(`${current}`)
-      await this.sleep(1000)
+    for (let current = from; current > 0; current--) {
+      this.display.displayText(`${current}`);
+      await this.sleep(1000);
     }
   }
 
   async start() {
-    await this.countdown()
-    for (let i=0; i<this.maxRounds; i++) {
-      this.display.displayText('')
+    await this.countdown();
+    for (let i = 0; i < this.maxRounds; i++) {
+      this.display.displayText("");
       await this.sleep(2000);
-      const { tookMs, matched } = await this.measureReactionTime()
-      
+      const { tookMs, matched } = await this.measureReactionTime();
+
       if (matched) {
         const roundsLeft = this.maxRounds - i - 1;
-        this.display.displayText(`CORRECT\n${tookMs}ms\n${roundsLeft} rounds left`)
+        this.display.displayText(
+          `CORRECT\n${tookMs}ms\n${roundsLeft} rounds left`
+        );
         await this.sleep(3000);
-        this.reactionTimesMs.push(tookMs)
+        this.reactionTimesMs.push(tookMs);
       } else {
         return null;
       }
     }
 
-    return this.reactionTimesMs
+    return this.reactionTimesMs;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 const connectBtn = document.getElementById("connectBtn");
+const resultEle = document.getElementById("result");
 const startBtn = document.getElementById("startBtn");
+const easyBtn = document.getElementById("easy");
+const hard = document.getElementById("hard");
+
 const setText = (el, text) => {
   el.innerText = text;
 };
@@ -10,20 +14,23 @@ connectBtn.addEventListener("click", () => {
 });
 
 startBtn.addEventListener("click", () => {
-  const game = new ReflexTimeGame(display, { rounds: 5 })
+  const game = new ReflexTimeGame(display, { rounds: 2 });
 
   game.start().then((result) => {
     if (result) {
-      const minReactionTimeMs = Math.min(...result)
-      const maxReactionTimeMs = Math.max(...result)
-      const avgReactionTimeMs = result.reduce((sum, reactionTime) => sum + reactionTime, 0) / result.length
+      const minReactionTimeMs = Math.min(...result);
+      const maxReactionTimeMs = Math.max(...result);
+      const avgReactionTimeMs =
+        result.reduce((sum, reactionTime) => sum + reactionTime, 0) /
+        result.length;
 
-      display.displayText(`MIN: ${minReactionTimeMs}ms\nMAX: ${maxReactionTimeMs}ms\nAVG: ${avgReactionTimeMs}ms`)
+      const text = `MIN: ${minReactionTimeMs}ms\nMAX: ${maxReactionTimeMs}ms\nAVG: ${avgReactionTimeMs}ms`;
+      display.displayText(text);
+      setText(resultEle, text);
     } else {
-      display.displayText('GAME OVER')
+      display.displayText("GAME OVER");
     }
-  })
-
+  });
 });
 
 display.onConnect(() => {
